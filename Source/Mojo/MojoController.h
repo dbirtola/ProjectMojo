fdc67cf7e4101fb1b7c686513bb231b1ee9c890a
Thelonius Erickson <theoserickson@gmail.com>
2021-06-12 15:10:16 -0700

    Added Inputs Bitmask to new MojoController class

---
+++ Source/Mojo/MojoController.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MojoController.generated.h"
+
+/**
+ * 
+ */
+//When the clip and/or ammo reserves are empty, what should happen?
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum class EInputDirection : uint8
+{
+	//A 0 entry required for compilation as an enum needs an explicit default value 
+	None = 0,
+    Forward = 1 << 0,
+    Right = 1 << 1,
+	Down = 1 << 2,
+	Left = 1 << 3
+};
+ENUM_CLASS_FLAGS(EInputDirection);
+
+UCLASS()
+class MOJO_API AMojoController : public APlayerController
+{
+	GENERATED_BODY()
+
+	protected:
+	EInputDirection InputDirection;
+};
